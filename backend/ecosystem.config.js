e68ab7f9b85ad7cc66d6917a7b24ab45ff0b8438
vartexxx <vartexxx29@yandex.ru>
2024-03-13 23:26:02 +0500

    fix: config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,6 +6,7 @@ const {
   DEPLOY_PATH,
   DEPLOY_FOLDER,
   DEPLOY_REF,
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -19,10 +20,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'git@github.com:vartexxx/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ~${DEPLOY_FOLDER} && npm i && npm run build && pm2 start`,
+      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
+      'post-deploy': 'npm i && npm run build && pm2 start',
     },
   },
 };
