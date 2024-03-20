84a04f58ededeedb9553b2c1f776ebf7d2a48c9d
vartexxx <vartexxx29@yandex.ru>
2024-03-20 16:52:38 +0500

    fix: ecosystem config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 start`,
     },
   },
 };
